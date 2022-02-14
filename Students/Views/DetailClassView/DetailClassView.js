41bcb6e841c47fd08788a3197dd1da8d60fb8f42
DangHuynh <danghuynh.dev@gmail.com>
2022-02-14 22:16:15 +0700

    ket noi page dang ky lich bao cao

--- Students/Views/DetailClassView/DetailClassView.js
+++ Students/Views/DetailClassView/DetailClassView.js
@@ -1,7 +1,10 @@
 $(".link-topic").click(function(){
     location.href = "../../Views/RegisTopicView/RegisTopicView.php"
 })
-
+$(".link-calen").click(function(){
+    location.href = "../../Views/CalenReportView/CalenReportView.php"
+    console.log("click");
+})
 var linkProcess = document.getElementsByClassName("link-title-process")
 for(i=0; i<linkProcess.length;i++){
     linkProcess[i].addEventListener('click', function(){
